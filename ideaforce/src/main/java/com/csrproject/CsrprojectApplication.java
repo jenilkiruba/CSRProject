9a9891ad875873708979e3c1bef10067c11f4a32
Vaibhav161994 <vaibhav.kaushik1694@gmail.com>
2018-11-26 11:00:33 +0530

    Initial Setup

---
+++ ideaforce/src/main/java/com/csrproject/CsrprojectApplication.java
@@ -0,0 +1,12 @@
+package com.csrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrprojectApplication.class, args);
+	}
+}
