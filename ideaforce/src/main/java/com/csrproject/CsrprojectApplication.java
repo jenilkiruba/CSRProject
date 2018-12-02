28fba57de13a36d644d132273f45805f9881fab2
Bharathiraja Subramanian <bharathiraja.subramanian@rsa.com>
2018-12-02 13:45:05 +0530

    Initial commit with ssl changes

--- ideaforce/src/main/java/com/csrproject/CsrprojectApplication.java
+++ ideaforce/src/main/java/com/csrproject/CsrprojectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CsrprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsrprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CsrprojectApplication.class, args);
+    }
 }
--- ideaforce/src/main/java/com/csrproject/controller/CommonController.java
+++ ideaforce/src/main/java/com/csrproject/controller/CommonController.java
@@ -1,31 +1,49 @@
 package com.csrproject.controller;
 
+import com.csrproject.entity.ChallengeEntity;
+import com.csrproject.repository.ChallengeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 /**
  * Created by kaushv5 on 11/25/2018.
  */
 @Controller
+@RequiredArgsConstructor
 public class CommonController {
 
+    @Autowired
+    private final ChallengeRepository challengeRepository;
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @RequestMapping("/home")
-    public String HomePage(){
+    public String home(Principal principal) {
         return "index";
     }
 
     @RequestMapping("/challenges")
-    public String Challenges(){
-        return "challenges";
+    public void challengesList(Principal principal, Model model) {
+        List<ChallengeEntity> challengeEntityList = challengeRepository.findAll();
+        model.addAttribute("challenges", challengeEntityList);
     }
 
     @RequestMapping("/gallery")
-    public String Gallery(){
+    public String gallery() {
         return "gallery";
     }
 
     @RequestMapping("/leaderboard")
-    public String LeaderBoard(){
+    public String LeaderBoard() {
         return "leaderboard";
     }
 }
