ecdef148a5d9c030f1144101d4ae27c6bfa69488
quailbr <3285245+quailbr@users.noreply.github.com>
2018-12-27 21:28:08 +0530

    Updated existing UI Pages (#8)
    * Updated Gallery and Leader board pages
    * Updated Profile and Logout

---
+++ ideaforce/src/main/resources/static/assets/js/gallery.js
@@ -0,0 +1,26 @@
+/**
+ * Created by kaushv5 on 12/4/2018.
+ */
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
