95f9c62ce90f06b4bcb2b23edf5d31760cbc8fa3
Bharathiraja Subramanian <bharathiraja.subramanian@rsa.com>
2018-12-27 21:20:24 +0530

    Removed unused classes

--- ideaforce/src/main/java/com/emc/ideaforce/repository/ChallengeEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emc.ideaforce.repository;
-
-import com.emc.ideaforce.model.ChallengeEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChallengeEntryRepository extends JpaRepository<ChallengeEntry, String> {
-}
--- ideaforce/src/main/java/com/emc/ideaforce/repository/ChallengeImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emc.ideaforce.repository;
-
-import com.emc.ideaforce.model.ChallengeImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChallengeImageRepository extends JpaRepository<ChallengeImage, String> {
-}
