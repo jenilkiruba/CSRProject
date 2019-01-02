21ca53b3be3a2caecae31cdbe719d67b4d609f8f
Bharathiraja Subramanian <bharathiraja.subramanian@rsa.com>
2019-01-02 15:33:05 +0530

    Fixed password validation issue

--- ideaforce/src/main/java/com/emc/ideaforce/controller/PasswordMatches.java
+++ ideaforce/src/main/java/com/emc/ideaforce/controller/PasswordMatches.java
@@ -3,6 +3,7 @@ package com.emc.ideaforce.controller;
 import javax.validation.Constraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -16,6 +17,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface PasswordMatches {
 
     String message() default "Passwords do not match";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 
 }
 
