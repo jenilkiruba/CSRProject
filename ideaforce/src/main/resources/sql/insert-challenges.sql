6ad343fb2f0ff645bde03a8e4bda8382db3718d4
Bharathiraja Subramanian <bharathiraja.subramanian@rsa.com>
2019-02-06 06:35:27 +0530

    Added challenges sql files

---
+++ ideaforce/src/main/resources/sql/insert-challenges.sql
@@ -0,0 +1,131 @@
+
+insert into challenges(id,name,description) values(1,'Energy Saving','<p><em>Conserve energy with one simple step.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Consume 60% less electricity by replacing traditional electrical bulbs in your home with alternative energy saving bulbs. </span></strong></p>');
+
+
+
+Insert into challenges(id,name,description) values(2,'Team Challenge','<p><em>Time to team up! </em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Get together with your teammates and upcycle 3 old items that you are not using anymore.</span></strong></p>');
+
+
+
+Insert into challenges(id,name,description) values(3,'Rainwater Harvesting','<p><em>Saving the world starts at home.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Set up a Rainwater Harvesting system in your home.</span></strong></p>');
+
+
+Insert into challenges(id,name,description) values(4,'Community Clean Up ','<p><em>Spot fix an area near you!</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Take your family or teammates to a nearby public spot and remove the litter from the area.</span></strong></p>');
+
+
+
+Insert into challenges(id,name,description) values(5,'Local Food','<p><em>Where does your food come from?</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Locate and recognize foods (vegetables, fruit, dairy and meat/seafood) that are produced near your neighborhood: especially those of season. In this way you will reduce pollution on account of transportation of food material.</span></strong></p>');
+
+
+
+Insert into challenges(id,name,description) values(6,'Tree Planting','<p><em>Plant trees.&nbsp; </em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">It is said that one hectare of forest eliminates in one year the carbon dioxide produced by four families during the same time. Plant 5 trees.</span></strong></p>');
+
+
+
+Insert into challenges(id,name,description) values(7,'Sustainable Fashion','<p><em>Develop a sustainable style.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Get together with your teammates and watch an eye-opening documentary on the fashion industry. <br>Or<br> Avoid clothes ending up in landfills and be a part of the Around the World &lsquo;Clothes Swap&rsquo;.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(8,'Sensitize Your Children','<p><em>Be a green role model.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Encourage your children to make simple changes to their everyday routine that will help them to have a greener footprint.</span></strong></p>');
+
+insert into challenges(id,name,description) values(9,'Plastic Straws','<p><em>Plastic straws are a major polluter of our oceans.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Carry with you an eco-friendly, reusable straw, or better yet, go strawless whenever you can.</span></strong></p>');
+
+insert into challenges(id,name,description) values(10,'B.Y.O.B(ottle)','<p><em>Say no to single-use cups and bottles.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Bring your own coffee mug and water bottles to the office during the campaign.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(11,'Composting at Home','<p><em>Turn your kitchen trash into treasure.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Composting garbage at home can be a useful way of recycling kitchen and yard waste. Set up your very own composting pit at home.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(12,'Green Gifting','<p><em>Gift someone a chance to go green!</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Gift an eco-friendly, reusable, recycled or upcycled product to your team mate or friends to help them go green.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(13,'Animals','<p><em>Let''s share the planet, it''s the only one we have.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Volunteer 5 hours of your time with an NGO dedicated to animal protection and conservation.</span></strong></p>');
+
+insert into challenges(id,name,description) values(14,'Waste','<p><em>Where does our waste end up?</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Visit a nearby landfill with your friends, teammates or family. Notice items which you have used in your everyday life and read about how long different products take to degenerate.</span></strong></p>');
+
+insert into challenges(id,name,description) values(15,'Eco-Friendly Gardening','<p><em>Say yes to organic alternatives, mulching and say no to chemicals.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Identify, access and use environmentally-friendly and organic alternatives to the chemical fertilizers and pesticides that you are using in your garden. These are better for plant growth and soil quality in the long run.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(16,'Renewable Energy','<p><em>Tap into solar power.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Install solar panels on your roofs to partially or fully supply power to your home.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(17,'Transport','<p><em>Shared mobility is the future.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Use mass transport, bicycles or carpooling once a week, for 3 weeks. This will reduce the usage of private vehicles, thereby decreasing pollution.</span></strong></p>');
+
+
+insert into challenges(id,name,description) values(18,'Greener Reading Habits','<p><em>Save money, ink and paper that goes into printing new books.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Borrow from libraries instead of buying personal books. Invest in e-books instead of print books.</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(19,'Meatless Monday','<p><em>How does meat in your diet take an environmental toll?</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">If you eat meat, add one meatless day a week for 3 weeks. Meat costs a lot at the store and it''s even more expensive when you consider its related environmental and health costs.</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(20,'e-Waste','<p><em>130 million cell phones are being thrown aside each year.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">The life span of mobile phones is usually 18 months, so recycle at least 2 old cell phones at a certified e-waste drop off center to ensure responsible disposal of your e-products.</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(21,'Stationary','<p><em>Create a Shared Reserve</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Place any unwanted, excess or reusable office stationary that you may have in our ‘Around the World’ supply bin so that it can be used by others.</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(22,'Save Paper','<p><em>Double up.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Configure your office printer or copy machine so it prints on both the sides of the paper. This reduces the amount of paper used by half!</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(23,'Waste Management','<p><em>A responsible way to dispose waste.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Segregate your waste by setting up three different bins for different kinds of waste. </span></strong></p>');
+
+
+insert into challenges(id,name,description) values(24,'Set up a Kitchen Garden','<p><em>Be your own farmer.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Set up a kitchen garden at home and grow 5 varieties of vegetables or herbs that you use every day.</span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(25,'Personal Care Products','<p><em>Do you know what your body care products are made of?</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Replace 5 of your personal products with alternatives which are devoid of harmful chemicals to your health and the environment such as Diethyl Phthalate, Triclosan, Cocoamidopropyl Betaine and Oxybenzone. </span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(26,'Cruelty Free Products','<p><em>Say no to animal testing.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Many daily use products have been unwittingly tested on animals. Identify and replace 5 products that you currently use with cruelty-free brands available in the market. </span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(27,'Responsible Consumption','<p><em>Are you making responsible choices when buying at supermarkets?</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">As consumers, we have the power to decide the kind of system we want. Identify 5 everyday products that fall under the category of both fair trade and non-GMO. Switch to these products and request your local supermarkets to carry non-GMO products. </span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(28,'School Challenge','<p><em>Help the future generation make sustainable choices.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Conduct a session with children in nearby schools to create awareness on current environmental issues, and how they can contribute to make a difference. </span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(29,'Backyard Wildlife Habitat','<p><em>Nurture nature’s habitants.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">A backyard wildlife habitat or "naturescape" can be created in your own backyard, patio or deck by incorporating a few basic elements. </span></strong></p>');
+
+
+
+insert into challenges(id,name,description) values(30,'Pack a Waste-Free Lunch','<p><em>The best way to reduce trash is to not create it.</em></p>
+<p><strong><span style="font-size: 13.0pt; font-family: ''Calibri'',sans-serif;">Pack a waste-free lunch for a week. Change the way you carry your lunch to work by using reusable products that won’t leave any waste behind apart from compostable items.</span></strong></p>');
